96fd741a2ce4b3555aefae6d8204ff064239459d
Base127 <muh.lillaqa@gmail.com>
2020-08-16 10:27:25 -0400

    Commit

--- src/module4/AlertConfirmation.java
+++ src/module4/AlertConfirmation.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AlertConfirmation {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "M:\\Lilla\\Installation_Stuff\\SeleniumFiles\\ExeFiles\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\TestEclipse\\Drivers\\chrome.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://seleniumautomationpractice.blogspot.com/2018/01/blog-post.html");
