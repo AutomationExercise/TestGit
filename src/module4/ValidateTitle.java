18bb25f39876cf34249e09c99ed8c0d78f838a3c
Base127 <muh.lillaqa@gmail.com>
2020-08-16 10:23:21 -0400

    learning purposes

--- src/module4/ValidateTitle.java
+++ src/module4/ValidateTitle.java
@@ -5,21 +5,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ValidateTitle {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "M:\\Lilla\\Installation_Stuff\\SeleniumFiles\\ExeFiles\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+		Thread.sleep(3000);
 		driver.get("https://www.google.com/");
 	
 		String GoogleTitle = driver.getTitle(); // Getting the title
-		
+	
 		System.out.println("This is " + GoogleTitle + " " +  "Title"); // printing the 'Google' title
 		
 		boolean Result = GoogleTitle.equals("Google"); // validating the 'Google' title is present
 		
 		System.out.println("Value is : " + Result); // printing the result
-		
+				
 		driver.close(); // closing the Window
 		
 		
